7b83ea966ae03ff5696c01824d61a84977adc115
Harumami <121877756+harumami@users.noreply.github.com>
2025-06-10 12:32:12 +0900

    Modify src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,20 @@ use {
         eyre::Result,
         install as install_eyre,
     },
+    ::ratatui::crossterm::terminal::enable_raw_mode,
     ::std::{
         io::stderr,
         process::ExitCode,
     },
+    ::tokio::{
+        runtime::Runtime,
+        signal::ctrl_c,
+        task::JoinHandle,
+    },
     ::tracing::{
         error,
         level_filters::LevelFilter,
+        trace,
     },
     ::tracing_appender::non_blocking::{
         NonBlocking,
@@ -17,55 +24,56 @@ use {
     },
     ::tracing_subscriber::{
         filter::EnvFilter,
-        fmt::Layer as FmtLayer,
+        fmt::Layer,
         layer::SubscriberExt as _,
-        registry::Registry as SpanRegistry,
+        registry::Registry,
         util::SubscriberInitExt as _,
     },
-    ::webrtc::api::APIBuilder,
-    tokio::signal::ctrl_c,
-    tracing::trace,
-    webrtc::{
+    ::webrtc::{
+        api::APIBuilder,
         ice_transport::ice_server::RTCIceServer,
         peer_connection::configuration::RTCConfiguration,
     },
+    ratatui::{
+        Terminal,
+        crossterm::{
+            execute,
+            terminal::{
+                EnterAlternateScreen,
+                LeaveAlternateScreen,
+                disable_raw_mode,
+            },
+        },
+        prelude::CrosstermBackend,
+    },
+    std::io::{
+        StdoutLock,
+        stdout,
+    },
 };
 
-#[tokio::main]
-async fn main() -> ExitCode {
-    match Result::Ok(()).and_then(|()| install_eyre()).and_then(|()| {
-        let (writer, guard) = NonBlocking::new(stderr());
-
-        SpanRegistry::default()
-            .with(FmtLayer::new().with_writer(writer))
-            .with(
-                EnvFilter::builder()
-                    .with_default_directive(LevelFilter::INFO.into())
-                    .from_env()?,
-            )
-            .try_init()?;
-
-        Result::Ok(guard)
-    }) {
-        Result::Ok(_guard) => {},
-        Result::Err(error) => eprintln!("{error}"),
-    }
-
-    match run().await {
-        Result::Ok(_guard) => ExitCode::SUCCESS,
+fn main() -> ExitCode {
+    match init() {
+        Result::Ok(_guard) => match run() {
+            Result::Ok(_handle) => ExitCode::SUCCESS,
+            Result::Err(error) => {
+                error!(error = &*error);
+                ExitCode::FAILURE
+            },
+        },
         Result::Err(error) => {
-            error!(error = &*error);
+            eprintln!("{error}");
             ExitCode::FAILURE
         },
     }
 }
 
-async fn run() -> Result<WorkerGuard> {
+fn init() -> Result<WorkerGuard> {
     install_eyre()?;
     let (writer, guard) = NonBlocking::new(stderr());
 
-    SpanRegistry::default()
-        .with(FmtLayer::new().with_writer(writer))
+    Registry::default()
+        .with(Layer::new().with_writer(writer))
         .with(
             EnvFilter::builder()
                 .with_default_directive(LevelFilter::INFO.into())
@@ -73,6 +81,16 @@ async fn run() -> Result<WorkerGuard> {
         )
         .try_init()?;
 
+    Result::Ok(guard)
+}
+
+fn run() -> Result<()> {
+    // let renderer =
+    let handle = Runtime::new()?.spawn(task());
+    Result::Ok(())
+}
+
+async fn task() -> Result<()> {
     let connection = APIBuilder::new()
         .build()
         .new_peer_connection(RTCConfiguration {
@@ -87,5 +105,39 @@ async fn run() -> Result<WorkerGuard> {
     trace!(%connection);
     ctrl_c().await?;
     connection.close().await?;
-    Result::Ok(guard)
+    Result::Ok(())
+}
+
+struct Renderer {
+    terminal: Terminal<CrosstermBackend<StdoutLock<'static>>>,
+}
+
+impl Renderer {
+    fn new() -> Result<Self> {
+        let mut renderer = Self {
+            terminal: Terminal::new(CrosstermBackend::new(stdout().lock()))?,
+        };
+
+        enable_raw_mode()?;
+        execute!(renderer.backend_mut(), EnterAlternateScreen)?;
+        Result::Ok(renderer)
+    }
+
+    fn backend_mut(&mut self) -> &mut CrosstermBackend<StdoutLock<'static>> {
+        self.terminal.backend_mut()
+    }
+
+    fn kill(&mut self) -> Result<()> {
+        disable_raw_mode()?;
+        execute!(self.backend_mut(), LeaveAlternateScreen)?;
+        Result::Ok(())
+    }
+}
+
+impl Drop for Renderer {
+    fn drop(&mut self) {
+        if let Result::Err(error) = self.kill() {
+            error!(error = &*error);
+        }
+    }
 }
