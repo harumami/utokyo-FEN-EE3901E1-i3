b7429c9e1d3eee0c29b3c83f074f89767fb04796
Harumami <121877756+harumami@users.noreply.github.com>
2025-06-11 16:17:25 +0900

    Modify files

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,69 @@
-mod defer;
-mod render;
-mod report;
-mod widget;
-
 use {
-    crate::{
-        render::Renderer,
-        report::Result,
+    ::ratatui::{
+        Terminal,
+        backend::CrosstermBackend,
+        buffer::Buffer,
+        crossterm::{
+            event::{
+                Event,
+                KeyCode,
+                KeyEvent,
+                KeyEventKind,
+                read,
+            },
+            execute,
+            terminal::{
+                EnterAlternateScreen,
+                LeaveAlternateScreen,
+                disable_raw_mode,
+                enable_raw_mode,
+            },
+        },
+        layout::Rect,
+        style::{
+            Color,
+            Modifier,
+        },
+        symbols::border::THICK,
+        text::{
+            Line,
+            Span,
+            Text,
+        },
+        widgets::{
+            Block,
+            Paragraph,
+            StatefulWidget,
+            Widget,
+        },
     },
     ::std::{
+        backtrace::Backtrace,
+        error::Error,
+        fmt::{
+            Display,
+            Formatter,
+            Result as FmtResult,
+        },
         fs::File,
-        io::BufWriter,
+        io::{
+            BufWriter,
+            stdout,
+        },
+        num::Saturating,
+        panic::{
+            set_hook,
+            take_hook,
+        },
         process::ExitCode,
-    },
-    ::tokio::{
-        runtime::Runtime,
-        signal::ctrl_c,
+        result::Result as StdResult,
     },
     ::tracing::{
+        debug,
+        error,
+        field::display,
+        info,
+        instrument,
         level_filters::LevelFilter,
         trace,
     },
@@ -25,23 +71,20 @@ use {
         NonBlocking,
         WorkerGuard,
     },
-    ::tracing_error::ErrorLayer,
+    ::tracing_error::{
+        ErrorLayer,
+        SpanTrace,
+    },
     ::tracing_subscriber::{
         filter::EnvFilter,
         fmt::{
             Layer,
-            format::DefaultFields,
+            format::FmtSpan,
         },
         layer::SubscriberExt as _,
         registry::Registry,
         util::SubscriberInitExt as _,
     },
-    ::webrtc::{
-        api::APIBuilder,
-        ice_transport::ice_server::RTCIceServer,
-        peer_connection::configuration::RTCConfiguration,
-    },
-    ratatui::Frame,
 };
 
 fn main() -> ExitCode {
@@ -70,44 +113,182 @@ fn init() -> Result<WorkerGuard> {
                 .with_default_directive(LevelFilter::INFO.into())
                 .from_env()?,
         )
-        .with(Layer::new().with_writer(writer))
-        .with(ErrorLayer::new(DefaultFields::new()))
+        .with(
+            Layer::new()
+                .with_writer(writer)
+                .with_thread_ids(true)
+                .with_thread_names(true)
+                .with_file(true)
+                .with_line_number(true)
+                .with_span_events(FmtSpan::FULL)
+                .with_ansi(false),
+        )
+        .with(ErrorLayer::default())
         .try_init()?;
 
     Result::Ok(guard)
 }
 
+#[instrument]
 fn run() -> Result<()> {
-    let mut renderer = Renderer::new()?;
-    let handle = Runtime::new()?.spawn(task());
+    debug!("take panic hook");
+    let hook = take_hook();
+    debug!("set panic hook");
 
-    renderer.terminal().draw(|frame| {
-        if let Result::Err(error) = draw(frame) {
-            error.report();
+    set_hook(Box::new(move |info| {
+        let payload = info
+            .payload()
+            .downcast_ref::<&str>()
+            .copied()
+            .or(info.payload().downcast_ref::<String>().map(String::as_str));
+
+        let location = info.location().map(display);
+        let backtrace = Backtrace::force_capture();
+        let spantrace = SpanTrace::capture();
+        error!(payload, location, %backtrace, %spantrace, "we panic");
+        hook(info);
+    }));
+
+    info!("enable raw mode");
+    enable_raw_mode()?;
+
+    let deferrer = Deferrer::new(|| {
+        info!("disable raw mode");
+        disable_raw_mode()?;
+        Result::Ok(())
+    });
+
+    let mut lock = stdout().lock();
+    info!("enter alternate screen");
+    execute!(lock, EnterAlternateScreen)?;
+
+    let _deferrer = Deferrer::new(|| {
+        drop(deferrer);
+        info!("leave alternate screen");
+        execute!(stdout(), LeaveAlternateScreen)?;
+        Result::Ok(())
+    });
+
+    let mut terminal = Terminal::new(CrosstermBackend::new(lock))?;
+    trace!(?terminal);
+    let mut counter = Saturating(0);
+    let mut exit = false;
+
+    while !exit {
+        terminal.draw(|frame| {
+            frame.render_stateful_widget(Counter, frame.area(), &mut counter.0);
+        })?;
+
+        if let Event::Key(KeyEvent {
+            code,
+            kind: KeyEventKind::Press,
+            ..
+        }) = read()?
+        {
+            match code {
+                KeyCode::Char('q') => {
+                    info!("exit loop");
+                    exit = true
+                },
+                KeyCode::Left => counter -= 1,
+                KeyCode::Right => counter += 1,
+                _ => (),
+            }
         }
-    })?;
+    }
 
     Result::Ok(())
 }
 
-fn draw(frame: &mut Frame<'_>) -> Result<()> {
-    Result::Ok(())
+struct Deferrer<F: FnOnce() -> Result<()>> {
+    f: Option<F>,
 }
 
-async fn task() -> Result<()> {
-    let connection = APIBuilder::new()
-        .build()
-        .new_peer_connection(RTCConfiguration {
-            ice_servers: vec![RTCIceServer {
-                urls: vec!["stun:stun.l.google.com:19302".to_owned()],
-                ..Default::default()
-            }],
-            ..Default::default()
-        })
-        .await?;
-
-    trace!(%connection);
-    ctrl_c().await?;
-    connection.close().await?;
-    Result::Ok(())
+impl<F: FnOnce() -> Result<()>> Deferrer<F> {
+    fn new(f: F) -> Self {
+        Self {
+            f: Option::Some(f),
+        }
+    }
 }
+
+impl<F: FnOnce() -> Result<()>> Drop for Deferrer<F> {
+    #[instrument(level = "debug", skip(self))]
+    fn drop(&mut self) {
+        if let Option::Some(f) = self.f.take() {
+            if let Result::Err(error) = f() {
+                error.report();
+            }
+        }
+    }
+}
+
+struct Counter;
+
+impl StatefulWidget for Counter {
+    type State = u32;
+
+    #[instrument(level = "trace", skip(self, buf))]
+    fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
+        let title = Line::styled(" Counter App Tutorial ", Modifier::BOLD);
+
+        let instructions = Line::from(vec![
+            Span::raw(" Decrement "),
+            Span::styled("<Left>", (Color::Blue, Modifier::BOLD)),
+            Span::raw(" Increment "),
+            Span::styled("<Right>", (Color::Blue, Modifier::BOLD)),
+            Span::raw(" Quit "),
+            Span::styled("<Q> ", (Color::Blue, Modifier::BOLD)),
+        ]);
+
+        let block = Block::bordered()
+            .title(title.centered())
+            .title_bottom(instructions.centered())
+            .border_set(THICK);
+
+        let counter_text = Text::from(vec![Line::from(vec![
+            Span::raw("Value: "),
+            Span::styled(state.to_string(), Color::Yellow),
+        ])]);
+
+        Paragraph::new(counter_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
+    }
+}
+
+struct Report {
+    error: Box<dyn Send + Error>,
+    backtrace: Backtrace,
+    spantrace: SpanTrace,
+}
+
+impl Display for Report {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        self.error.fmt(f)?;
+        f.write_str("\n\n")?;
+        self.backtrace.fmt(f)?;
+        f.write_str("\n\n")?;
+        self.spantrace.fmt(f)?;
+        Result::Ok(())
+    }
+}
+
+impl Report {
+    fn report(&self) {
+        error!(error = self.error, backtrace = %self.backtrace, spantrace = %self.spantrace);
+    }
+}
+
+impl<E: 'static + Send + Error> From<E> for Report {
+    fn from(value: E) -> Self {
+        Self {
+            error: Box::new(value),
+            backtrace: Backtrace::force_capture(),
+            spantrace: SpanTrace::capture(),
+        }
+    }
+}
+
+type Result<T, E = Report> = StdResult<T, E>;
