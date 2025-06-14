7e4fcbd922bc7bccc026650f6f751011942e4b79
Harumami <121877756+harumami@users.noreply.github.com>
2025-06-14 16:24:10 +0900

    Modify files

--- build.rs
+++ build.rs
@@ -9,8 +9,23 @@ use {
 fn main() {
     println!("cargo:rerun-if-changed=vcpkg.json");
 
+    let arch = match var("CARGO_CFG_TARGET_ARCH").unwrap().as_str() {
+        "x86_64" => "x64",
+        "aarch64" => "arm64",
+        arch => panic!("unsupported arch: {arch}"),
+    };
+
+    let os = match var("CARGO_CFG_TARGET_OS").unwrap().as_str() {
+        "windows" => "windows-static-md",
+        "macos" => "osx",
+        "linux" => "linux",
+        os => panic!("unsupported os: {os}"),
+    };
+
+    let triplet = format!("{arch}-{os}");
+
     let status = Command::new("vcpkg")
-        .args(["install"])
+        .args(["install", "--triplet", &triplet])
         .output()
         .expect("failed to spawn vcpkg command")
         .status;
@@ -21,27 +36,25 @@ fn main() {
         status.code()
     );
 
-    let arch = match var("CARGO_CFG_TARGET_ARCH").unwrap().as_str() {
-        "x86_64" => "x64",
-        "aarch64" => "arm64",
-        arch => panic!("unsupported arch: {arch}"),
-    };
-
-    let os = match var("CARGO_CFG_TARGET_OS").unwrap().as_str() {
-        "windows" => "windows",
-        "macos" => "osx",
-        "linux" => "linux",
-        os => panic!("unsupported os: {os}"),
-    };
-
     builder()
         .raw_line("#![allow(dead_code, non_camel_case_types)]")
-        .header(format!("vcpkg_installed/{arch}-{os}/include/opus/opus.h"))
+        .header(format!("vcpkg_installed/{triplet}/include/opus/opus.h"))
         .generate()
         .expect("failed to generate bindgen")
         .write_to_file("src/opus.rs")
         .expect("failed to write to file");
 
-    println!("cargo:rustc-link-search=native=vcpkg_installed/{arch}-{os}/lib");
+    builder()
+        .raw_line("#![allow(dead_code, non_camel_case_types)]")
+        .header(format!(
+            "vcpkg_installed/{triplet}/include/speex/speex_resampler.h"
+        ))
+        .generate()
+        .expect("failed to generate bindgen")
+        .write_to_file("src/speex.rs")
+        .expect("failed to write to file");
+
+    println!("cargo:rustc-link-search=native=vcpkg_installed/{triplet}/lib");
     println!("cargo:rustc-link-lib=static=opus");
+    println!("cargo:rustc-link-lib=static=speexdsp");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 mod opus;
+mod speex;
 
 use {
-    crate::opus::{
-        OPUS_APPLICATION_VOIP,
-        OPUS_OK,
-        OpusEncoder,
-        opus_encode,
-        opus_encoder_create,
-        opus_encoder_destroy,
+    crate::{
+        opus::{
+            OPUS_APPLICATION_VOIP,
+            OpusDecoder,
+            OpusEncoder,
+            opus_decode,
+            opus_decoder_create,
+            opus_decoder_destroy,
+            opus_encode,
+            opus_encoder_create,
+            opus_encoder_destroy,
+            opus_strerror,
+        },
+        speex::{
+            RESAMPLER_ERR_SUCCESS,
+            SpeexResamplerState,
+            speex_resampler_destroy,
+            speex_resampler_init,
+            speex_resampler_process_interleaved_int,
+            speex_resampler_strerror,
+        },
     },
     ::clap::{
         Parser,
@@ -25,6 +40,7 @@ use {
     },
     ::cpal::{
         Data,
+        FromSample,
         SampleFormat,
         SizedSample,
         platform::{
@@ -39,6 +55,7 @@ use {
     },
     ::crossbeam::queue::ArrayQueue,
     ::dasp::{
+        frame::Frame,
         sample::{
             Sample,
             ToSample,
@@ -63,6 +80,10 @@ use {
     ::std::{
         cmp::Reverse,
         error::Error,
+        ffi::{
+            CStr,
+            c_int,
+        },
         io::{
             BufWriter,
             stderr,
@@ -72,8 +93,15 @@ use {
         sync::Arc,
     },
     ::tokio::{
-        sync::Notify,
-        task::spawn_blocking,
+        signal::ctrl_c,
+        sync::{
+            Notify,
+            RwLock,
+        },
+        task::{
+            spawn,
+            spawn_blocking,
+        },
     },
     ::tracing::{
         debug,
@@ -95,12 +123,6 @@ use {
         registry::Registry,
         util::SubscriberInitExt as _,
     },
-    cpal::{
-        available_hosts,
-        host_from_id,
-    },
-    dasp::interpolate::linear::Linear,
-    std::collections::VecDeque,
 };
 
 #[tokio::main]
@@ -159,34 +181,6 @@ async fn run(command: Command) -> Result<()> {
     match command {
         Command::Generate => {
             println!("{}", generate());
-
-            for host in available_hosts() {
-                println!("Host: {}", host.name());
-                let host = host_from_id(host)?;
-
-                for device in host.devices()? {
-                    println!("Device: {}", device.name()?);
-
-                    for input_config in device.supported_input_configs()? {
-                        println!("Input Config: {input_config:?}");
-                    }
-
-                    for output_config in device.supported_output_configs()? {
-                        println!("Output Config: {output_config:?}");
-                    }
-
-                    if device.supports_input() {
-                        println!("Default Input Config: {:?}", device.default_input_config()?);
-                    }
-
-                    if device.supports_output() {
-                        println!(
-                            "Default Output Config: {:?}",
-                            device.default_output_config()?
-                        );
-                    }
-                }
-            }
         },
         Command::Host {
             secret,
@@ -270,28 +264,66 @@ async fn bind(secret: Option<SecretKey>, alpn: &[u8]) -> Result<Endpoint> {
 
 #[instrument]
 async fn commute(send_stream: SendStream, recv_stream: RecvStream) -> Result<()> {
-    let host = default_host();
+    let host = Arc::new(default_host());
     trace!(host = ?host.id());
     let sample_rate = 48000;
     let channels = 2;
+    let frame_size = sample_rate as usize * 20 / 1000;
+    let max_frame_size = sample_rate as usize * 120 / 1000;
+    let max_packet_size = 4000;
+    let quality = 5;
+    let exit = Arc::new(RwLock::new(false));
 
-    if let Option::Some(output_device) = host.default_output_device() {}
+    let record_handle = spawn(record(
+        send_stream,
+        host.clone(),
+        sample_rate,
+        channels,
+        frame_size,
+        max_packet_size,
+        quality,
+        exit.clone(),
+    ));
 
-    host.default_input_device();
-    info!("start communication");
+    let play_handle = spawn(play(
+        recv_stream,
+        host,
+        sample_rate,
+        channels,
+        max_frame_size,
+        max_packet_size,
+        quality,
+        exit.clone(),
+    ));
+
+    ctrl_c().await?;
+    *exit.write().await = true;
+
+    if let Result::Err(error) = record_handle.await? {
+        eprintln!("{error}");
+    }
+
+    if let Result::Err(error) = play_handle.await? {
+        eprintln!("{error}");
+    }
+
     Result::Ok(())
 }
 
+#[allow(clippy::too_many_arguments)]
 #[instrument(skip(host))]
-async fn record(send_stream: SendStream, host: Host) -> Result<()> {
-    const SAMPLE_RATE: usize = 48000;
-    const CHANNELS: usize = 2;
-    const FRAME_SIZE: usize = 960;
-    const CAPACITY: usize = 16 * FRAME_SIZE;
-    const MAX_PACKET_SIZE: usize = 4000;
+async fn record(
+    mut send_stream: SendStream,
+    host: Arc<Host>,
+    sample_rate: u32,
+    channels: u32,
+    frame_size: usize,
+    max_packet_size: usize,
+    quality: u32,
+    exit: Arc<RwLock<bool>>,
+) -> Result<()> {
     let device = host.default_input_device().ok_or_eyre("no input device")?;
     trace!(device = device.name()?);
-
     let mut configs = device.supported_input_configs()?.collect::<Vec<_>>();
 
     configs.sort_by_key(|config| {
@@ -318,8 +350,8 @@ async fn record(send_stream: SendStream, host: Host) -> Result<()> {
         _ => bail!("no input configuration which is stereo or mono"),
     };
 
-    let sample_rate = config.sample_rate().0;
-    let buffer0 = Arc::new(RingBuffer::new(CAPACITY));
+    let raw_sample_rate = config.sample_rate().0;
+    let buffer0 = Arc::new(RingBuffer::new(16 * frame_size));
     let buffer1 = buffer0.clone();
 
     let input_stream = spawn_blocking(move || {
@@ -347,26 +379,29 @@ async fn record(send_stream: SendStream, host: Host) -> Result<()> {
     .await??;
 
     input_stream.play()?;
-    let mut encoder = Encoder::new(SAMPLE_RATE, CHANNELS)?;
+    let mut buffer2 = Vec::new();
+    let mut resampler = Resampler::new(channels, raw_sample_rate, sample_rate, quality)?;
+    let mut encoder = Encoder::new(sample_rate, channels)?;
 
-    loop {
+    while *exit.read().await {
         buffer1.wait().await;
+        buffer2.extend(buffer1.drain().flatten());
+        let (n, buffer3) = resampler.resample(&buffer2)?;
+        buffer2.drain(0..(n * channels) as usize);
+        encoder.input().extend(buffer3);
 
-        encoder.input().extend(
-            from_iter(buffer1.drain())
-                .from_hz_to_hz(
-                    Linear::new([0; 2], [0; 2]),
-                    sample_rate as _,
-                    SAMPLE_RATE as _,
-                )
-                .until_exhausted()
-                .flatten(),
-        );
+        while encoder.ready(frame_size) {
+            encoder.encode(frame_size, max_packet_size)?;
 
-        while encoder.ready(FRAME_SIZE) {
-            encoder.encode(FRAME_SIZE, MAX_PACKET_SIZE)?;
+            if !encoder.output.is_empty() {
+                let len = (encoder.output().len() as u32).to_le_bytes();
+                send_stream.write_all(&len).await?;
+                send_stream.write_all(encoder.output()).await?;
+            }
         }
     }
+
+    Result::Ok(())
 }
 
 fn data_to_frames<S: 'static + SizedSample + ToSample<i16>>(
@@ -394,6 +429,122 @@ fn data_to_frames<S: 'static + SizedSample + ToSample<i16>>(
                 .into_iter()
                 .flatten(),
         ),
+    }
+}
+
+#[allow(clippy::too_many_arguments)]
+#[instrument(skip(host))]
+async fn play(
+    mut recv_stream: RecvStream,
+    host: Arc<Host>,
+    sample_rate: u32,
+    channels: u32,
+    max_frame_size: usize,
+    max_packet_size: usize,
+    quality: u32,
+    exit: Arc<RwLock<bool>>,
+) -> Result<()> {
+    let device = host
+        .default_output_device()
+        .ok_or_eyre("no output device")?;
+
+    trace!(device = device.name()?);
+    let mut configs = device.supported_output_configs()?.collect::<Vec<_>>();
+
+    configs.sort_by_key(|config| {
+        Reverse((
+            match config.channels() {
+                1 => 1,
+                2 => 2,
+                _ => 0,
+            },
+            config.max_sample_rate(),
+        ))
+    });
+
+    let config = configs
+        .first()
+        .ok_or_eyre("no output configuration")?
+        .with_max_sample_rate();
+
+    trace!(?config);
+
+    let stereo = match config.channels() {
+        1 => false,
+        2 => true,
+        _ => bail!("no output configuration which is stereo or mono"),
+    };
+
+    let raw_sample_rate = config.sample_rate().0;
+    let buffer0 = Arc::new(RingBuffer::new(16 * max_frame_size));
+    let buffer1 = buffer0.clone();
+
+    let output_stream = spawn_blocking(move || {
+        device.build_output_stream_raw(
+            &config.config(),
+            config.sample_format(),
+            move |data, _| match data.sample_format() {
+                SampleFormat::I8 => frames_to_data::<i8>(data, buffer0.drain(), stereo),
+                SampleFormat::I16 => frames_to_data::<i16>(data, buffer0.drain(), stereo),
+                SampleFormat::I24 => frames_to_data::<I24>(data, buffer0.drain(), stereo),
+                SampleFormat::I32 => frames_to_data::<i32>(data, buffer0.drain(), stereo),
+                SampleFormat::I64 => frames_to_data::<i64>(data, buffer0.drain(), stereo),
+                SampleFormat::U8 => frames_to_data::<u8>(data, buffer0.drain(), stereo),
+                SampleFormat::U16 => frames_to_data::<u16>(data, buffer0.drain(), stereo),
+                SampleFormat::U32 => frames_to_data::<u32>(data, buffer0.drain(), stereo),
+                SampleFormat::U64 => frames_to_data::<u64>(data, buffer0.drain(), stereo),
+                SampleFormat::F32 => frames_to_data::<f32>(data, buffer0.drain(), stereo),
+                SampleFormat::F64 => frames_to_data::<f64>(data, buffer0.drain(), stereo),
+                _ => (),
+            },
+            |error| error!(error = &error as &dyn Error),
+            Option::None,
+        )
+    })
+    .await??;
+
+    output_stream.play()?;
+    let mut buffer2 = Vec::new();
+    let mut decoder = Decoder::new(sample_rate, channels)?;
+    let mut resampler = Resampler::new(channels, sample_rate, raw_sample_rate, quality)?;
+
+    while *exit.read().await {
+        let mut buffer = [0; 4];
+        recv_stream.read_exact(&mut buffer).await?;
+        decoder.input().resize(u32::from_le_bytes(buffer) as _, 0);
+        recv_stream.read_exact(decoder.input()).await?;
+        decoder.decode(max_frame_size)?;
+        buffer2.extend_from_slice(decoder.output());
+        let (n, buffer3) = resampler.resample(&buffer2)?;
+        buffer2.drain(0..n as usize);
+        buffer1.extend(buffer3.chunks(2).map(|frame| [frame[0], frame[1]]));
+    }
+
+    Result::Ok(())
+}
+
+fn frames_to_data<S: 'static + SizedSample + Frame + FromSample<i16>>(
+    data: &mut Data,
+    frames: impl Iterator<Item = [i16; 2]>,
+    stereo: bool,
+) {
+    let samples = match stereo {
+        true => &mut frames.flatten() as &mut dyn Iterator<Item = _>,
+        false => &mut frames.map(|[sample0, sample1]| (sample0 + sample1) / 2)
+            as &mut dyn Iterator<Item = _>,
+    };
+
+    let mut signal = from_iter(samples).map(Sample::from_sample);
+
+    #[allow(unused_mut)]
+    for mut sample in match data.as_slice_mut::<S>() {
+        Option::Some(data) => data,
+        Option::None => {
+            warn!("invalid type for given data");
+            &mut []
+        },
+    } {
+        *sample = signal.next();
     }
 }
 
@@ -448,18 +599,85 @@ impl RingBuffer {
     }
 }
 
+struct Resampler {
+    raw: *mut SpeexResamplerState,
+    channels: u32,
+    in_rate: u32,
+    out_rate: u32,
+    out_buffer: Vec<i16>,
+}
+
+impl Resampler {
+    fn new(channels: u32, in_rate: u32, out_rate: u32, quality: u32) -> Result<Self> {
+        let mut error = 0;
+
+        let raw =
+            unsafe { speex_resampler_init(channels, in_rate, out_rate, quality as _, &mut error) };
+
+        Self::handle_error(error)?;
+
+        Result::Ok(Self {
+            raw,
+            channels,
+            in_rate,
+            out_rate,
+            out_buffer: Vec::new(),
+        })
+    }
+
+    fn resample(&mut self, in_buffer: &[i16]) -> Result<(u32, &[i16])> {
+        self.out_buffer.resize(
+            in_buffer.len() * self.out_rate as usize / self.in_rate as usize + 1,
+            0,
+        );
+
+        let mut in_len = in_buffer.len() as u32 / self.channels;
+        let mut out_len = self.out_buffer.len() as u32 / self.channels;
+
+        let error = unsafe {
+            speex_resampler_process_interleaved_int(
+                self.raw,
+                in_buffer.as_ptr(),
+                &mut in_len,
+                self.out_buffer.as_mut_ptr(),
+                &mut out_len,
+            )
+        };
+
+        Self::handle_error(error)?;
+        Result::Ok((in_len, &self.out_buffer[0..(self.channels * out_len) as _]))
+    }
+
+    fn handle_error(error: c_int) -> Result<()> {
+        if error == RESAMPLER_ERR_SUCCESS {
+            let error = unsafe { CStr::from_ptr(speex_resampler_strerror(error)) }.to_str()?;
+            bail!("speex error: {error}");
+        }
+
+        Result::Ok(())
+    }
+}
+
+impl Drop for Resampler {
+    fn drop(&mut self) {
+        unsafe { speex_resampler_destroy(self.raw) };
+    }
+}
+
+unsafe impl Send for Resampler {}
+
 struct Encoder {
-    opus: *mut OpusEncoder,
-    channels: usize,
+    raw: *mut OpusEncoder,
+    channels: u32,
     input: Vec<i16>,
     output: Vec<u8>,
 }
 
 impl Encoder {
-    fn new(sample_rate: usize, channels: usize) -> Result<Self> {
+    fn new(sample_rate: u32, channels: u32) -> Result<Self> {
         let mut error = 0;
 
-        let opus = unsafe {
+        let raw = unsafe {
             opus_encoder_create(
                 sample_rate as _,
                 channels as _,
@@ -468,10 +686,10 @@ impl Encoder {
             )
         };
 
-        ensure!(error == OPUS_OK as _, "failed to create opus encoder");
+        handle_opus_error(error)?;
 
         Result::Ok(Self {
-            opus,
+            raw,
             channels,
             input: Vec::new(),
             output: Vec::new(),
@@ -487,7 +705,7 @@ impl Encoder {
     }
 
     fn ready(&self, frame_size: usize) -> bool {
-        self.input.len() >= self.channels * frame_size
+        self.input.len() >= self.channels as usize * frame_size
     }
 
     fn encode(&mut self, frame_size: usize, max_packet_size: usize) -> Result<()> {
@@ -496,7 +714,7 @@ impl Encoder {
 
         let n = unsafe {
             opus_encode(
-                self.opus,
+                self.raw,
                 self.input.as_ptr(),
                 frame_size as _,
                 self.output.as_mut_ptr(),
@@ -504,8 +722,8 @@ impl Encoder {
             )
         };
 
-        ensure!(n >= 0, "failed to encode opus");
-        self.input.drain(0..self.channels * frame_size);
+        handle_opus_error(n)?;
+        self.input.drain(0..self.channels as usize * frame_size);
         self.output.truncate(n as _);
         Result::Ok(())
     }
@@ -513,6 +731,77 @@ impl Encoder {
 
 impl Drop for Encoder {
     fn drop(&mut self) {
-        unsafe { opus_encoder_destroy(self.opus) };
+        unsafe { opus_encoder_destroy(self.raw) };
     }
+}
+
+unsafe impl Send for Encoder {}
+
+struct Decoder {
+    raw: *mut OpusDecoder,
+    channels: u32,
+    input: Vec<u8>,
+    output: Vec<i16>,
+}
+
+impl Decoder {
+    fn new(sample_rate: u32, channels: u32) -> Result<Self> {
+        let mut error = 0;
+        let raw = unsafe { opus_decoder_create(sample_rate as _, channels as _, &mut error) };
+        handle_opus_error(error)?;
+
+        Result::Ok(Self {
+            raw,
+            channels,
+            input: Vec::new(),
+            output: Vec::new(),
+        })
+    }
+
+    fn input(&mut self) -> &mut Vec<u8> {
+        &mut self.input
+    }
+
+    fn output(&self) -> &[i16] {
+        &self.output
+    }
+
+    fn decode(&mut self, max_frame_size: usize) -> Result<()> {
+        self.output.resize(max_frame_size, 0);
+
+        let frames = unsafe {
+            opus_decode(
+                self.raw,
+                self.input.as_ptr(),
+                self.input.len() as _,
+                self.output.as_mut_ptr(),
+                max_frame_size as _,
+                0,
+            )
+        };
+
+        handle_opus_error(frames)?;
+
+        self.output
+            .truncate(self.channels as usize * frames as usize);
+
+        Result::Ok(())
+    }
+}
+
+impl Drop for Decoder {
+    fn drop(&mut self) {
+        unsafe { opus_decoder_destroy(self.raw) };
+    }
+}
+
+unsafe impl Send for Decoder {}
+
+fn handle_opus_error(error: c_int) -> Result<()> {
+    if error < 0 {
+        let error = unsafe { CStr::from_ptr(opus_strerror(error)) }.to_str()?;
+        bail!("opus error: {error}");
+    }
+
+    Result::Ok(())
 }
