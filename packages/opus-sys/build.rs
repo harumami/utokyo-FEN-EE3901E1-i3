fe34743fb610d6b6b0da754321a85945785ef658
Harumami <121877756+harumami@users.noreply.github.com>
2025-06-21 18:32:10 +0900

    Use CMake

--- packages/opus-sys/build.rs
+++ packages/opus-sys/build.rs
@@ -2,35 +2,27 @@
 use ::bindgen::CargoCallbacks;
 use {
     ::bindgen::builder,
+    ::cmake::build,
     ::std::env::var,
 };
 
 fn main() {
-    let arch = match var("CARGO_CFG_TARGET_ARCH").unwrap().as_str() {
-        "x86_64" => "x64",
-        "aarch64" => "arm64",
-        arch => panic!("unsupported arch: {arch}"),
-    };
-
-    let os = match var("CARGO_CFG_TARGET_OS").unwrap().as_str() {
-        "windows" => "windows-static-md",
-        "macos" => "osx",
-        "linux" => "linux",
-        os => panic!("unsupported os: {os}"),
-    };
-
-    let triplet = format!("{arch}-{os}");
+    let dist = build(".");
+    let mut header = dist.clone();
+    header.extend(["include", "opus", "opus.h"]);
+    let header = header.into_os_string().into_string().unwrap();
+    let mut lib = dist;
+    lib.extend(["lib"]);
+    let lib = lib.into_os_string().into_string().unwrap();
 
     builder()
-        .header(format!(
-            "../../vcpkg_installed/{triplet}/include/opus/opus.h"
-        ))
+        .header(header)
         .parse_callbacks(Box::new(CargoCallbacks::new()))
         .generate()
         .expect("failed to generate bindgen")
         .write_to_file(format!("{}/lib.rs", var("OUT_DIR").unwrap()))
         .expect("failed to write to file");
 
-    println!("cargo:rustc-link-search=native=vcpkg_installed/{triplet}/lib");
+    println!("cargo:rustc-link-search=native={lib}");
     println!("cargo:rustc-link-lib=static=opus");
 }
