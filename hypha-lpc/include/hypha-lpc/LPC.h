7330798f9fe6c577c0c6521352e9f96c063e2651
Harumami <121877756+harumami@users.noreply.github.com>
2025-06-26 14:42:43 +0900

    Modify hypha-lpc

--- hypha-lpc/include/hypha-lpc/LPC.h
+++ hypha-lpc/include/hypha-lpc/LPC.h
@@ -1,7 +1,18 @@
-#ifndef LP_H
-#define LP_H
+#ifndef HYPHA_LPC_LPC_H
+#define HYPHA_LPC_LPC_H
 
-void encode(float *p_as, float *p_ss);
-void decode(float *p_as, float *p_es);
+#define P 5
+#define N 20
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void encode(float p_as[P], float p_ss[N]);
+void decode(const float p_as[P], float p_es[N]);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- hypha-lpc/src/LPC.cpp
+++ hypha-lpc/src/LPC.cpp
@@ -1,71 +1,76 @@
 #include <Eigen/Dense>
 #include <cmath>
-
-constexpr const int p = 5;
-constexpr const int N = 20;
+#include <hypha-lpc/LPC.h>
+#include <span>
 
 // 配列長20の16bit符号付き整数配列のポインタをraw_dataにいれ、encode(data)でdataがLPC符号化される
 // encoded_dataには配列長25の不動点小数配列のポインタをいれる。
 // 実行後のencoded_dataを送る。
-extern "C" void encode(float (&p_as)[p], float (&p_ss)[N]) {
-    Eigen::Vector<float, p + 1> r;
+extern "C" void encode(float p_as[P], float p_ss[N]) {
+    std::span<float, P> as_span(p_as, P);
+    std::span<float, N> ss_span(p_ss, N);
+    Eigen::Vector<float, P + 1> r;
 
-    for (int i = 0; i < p + 1; i++) {
+    for (size_t i = 0; i < P + 1; i++) {
         float sum = 0;
 
-        for (int n = i; n < N; n++) {
+        for (size_t n = i; n < N; n++) {
             if (n >= i) {
-                sum += p_ss[n] * p_ss[n - i];
+                sum += ss_span[n] * ss_span[n - i];
             }
         }
 
-        r(i) = sum;
+        r(static_cast<int>(i)) = sum;
     }
 
-    Eigen::Matrix<float, p, p> R;
+    Eigen::Matrix<float, P, P> R;
 
-    for (int i = 0; i < p; i++) {
-        for (int j = 0; j < p; j++) {
+    for (int i = 0; i < P; i++) {
+        for (int j = 0; j < P; j++) {
             R(i, j) = r(abs(i - j));
         }
     }
 
-    Eigen::Vector<float, p> minus_a = R.inverse() * r.tail(p);
-    float e[N];
+    Eigen::Vector<float, P> minus_a = R.inverse() * r.tail(P);
+    std::array<float, N> e{};
 
-    for (int n = 0; n < N; n++) {
+    for (size_t n = 0; n < N; n++) {
         int as = 0;
 
-        for (int i = 0; i < p; i++) {
+        for (size_t i = 0; i < P; i++) {
             if (n > i) {
-                as += static_cast<int>(minus_a(i) * p_ss[n - i - 1]);
+                as += static_cast<int>(
+                    minus_a(static_cast<int>(i)) * ss_span[n - i - 1]
+                );
             }
         }
 
-        e[n] = p_ss[n] - static_cast<float>(as);
+        e[n] = ss_span[n] - static_cast<float>(as);
     }
 
-    std::copy_n(minus_a.data(), p, &p_as[0]);
-    std::copy_n(&e[0], N, &p_ss[0]);
+    std::copy_n(minus_a.data(), P, as_span.data());
+    std::copy_n(e.data(), N, ss_span.data());
 }
 
 // 送られてきたデータをencoded_dataに入れる。
 // 予め作成した配列長20の不動点小数配列を用意しておき、decoded_dataに入れる。
 // 実行後のdecoded_dataを受け取る。
-extern "C" void decode(float (&p_as)[p], float (&p_es)[N]) {
-    float s[N];
+extern "C" void decode(const float p_as[P], float p_es[N]) {
+    std::span<const float, P> as_span(p_as, P);
+    std::span<float, N> es_span(p_es, N);
+    std::array<float, N> s{};
 
-    for (int n = 0; n < N; n++) {
+    for (size_t n = 0; n < N; n++) {
         int as = 0;
 
-        for (int i = 0; i < p; i++) {
+        for (size_t i = 0; i < P; i++) {
             if (n > i) {
-                as += static_cast<int>(p_as[i] * s[n - i - 1]);
+                as += static_cast<int>(as_span[i] * s[n - i - 1]);
             }
         }
 
-        s[n] = p_es[n] + static_cast<float>(as);
+        s[n] = es_span[n] + static_cast<float>(as);
     }
 
-    std::copy_n(&s[0], N, &p_es[0]);
+    std::copy_n(s.data(), N, es_span.data());
 }
